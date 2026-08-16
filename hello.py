b84ff77b85afd4bd67b2f44821bbe8831ec709f6
silverhealthleader <silverhealthleader@gmail.com>
2026-08-17 00:31:39 +0900

    프로젝트 기본 구조 생성

---
+++ hello.py
@@ -0,0 +1,2 @@
+print("Hello")
+print("Python 프로그램이 정상적으로 실행되었습니다.")
---
+++ prompt_manager.py
@@ -0,0 +1,7 @@
+def main():
+    print("나만의 프롬프트 관리 프로그램")
+    print("프로그램 개발을 시작합니다.")
+
+
+if __name__ == "__main__":
+    main()
